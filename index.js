c104089942722416d758b90779673e33d2b7ec86
austinedavid <davmesoaustine@gmail.com>
2025-07-16 20:14:54 +0100

    working now

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-// const userRoutes = require("./routes/user.route");
-// const postRoute = require("./routes/post.route");
-// const KycRoute = require("./routes/kyc.route");
-// const bookRoute = require("./routes/book.route");
+const userRoutes = require("./routes/user.route");
+const postRoute = require("./routes/post.route");
+const KycRoute = require("./routes/kyc.route");
+const bookRoute = require("./routes/book.route");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
@@ -11,9 +11,7 @@ const app = express();
 
 // create connection
 mongoose
-  .connect(
-    "mongodb+srv://chatting:chatting@cluster0.vbqf51r.mongodb.net/axia8?retryWrites=true&w=majority"
-  )
+  .connect(process.env.MONGO_URL)
   .then(() => console.log("connection was successful"))
   .catch((error) => console.log(error.message));
 
@@ -24,10 +22,10 @@ const Port = process.env.PORT || 5000;
 app.use(cookieParser());
 // connect the mongoose below here
 // creating endpoints for the student database manipulation
-// app.use(userRoutes);
-// app.use(postRoute);
-// app.use(KycRoute);
-// app.use(bookRoute);
+app.use(userRoutes);
+app.use(postRoute);
+app.use(KycRoute);
+app.use(bookRoute);
 
 app.get("/", (req, res) => {
   return res.send("app is running");
